4e38e50f02bd780062b393b2d1f985ac4fff78df
Micke <micke@home.com>
2015-06-26 21:29:34 +0200

    Initial Commit

---
+++ BastuEnhet/BastuEnhet.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BastuEnhet.ino
+ Created:	6/26/2015 9:29:01 PM
+ Author:	Micke
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
